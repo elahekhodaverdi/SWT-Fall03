547d502d0cdf71fadab305404715ff77d114bd9b
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-10-14 17:39:41 +0330

    add rating test

--- src/test/java/mizdooni/model/RatingTest.java
+++ src/test/java/mizdooni/model/RatingTest.java
@@ -1,4 +1,25 @@
 package mizdooni.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RatingTest {
+
+    private Rating rating;
+
+    @BeforeEach
+    void setup() {
+        rating = new Rating();
+    }
+
+    @Test
+    void testGetStarCount() {
+        rating.food = 3;
+        rating.service = 3.5;
+        rating.ambiance = 3.75;
+        rating.overall = 3.5;
+        assertEquals(4, rating.getStarCount());
+    }
 }
