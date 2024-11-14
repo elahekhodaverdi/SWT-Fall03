05ceaa469f3b96a6d7d9689ceb22ac488062a757
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-11-14 11:44:13 +0330

    add test for transaction

--- Fesadyab/src/test/java/domain/TransactionTest.java
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -1,4 +1,41 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TransactionTest {
+    private Transaction transaction;
+
+    @BeforeEach
+    void setUp() {
+        transaction = new Transaction() {{
+            setTransactionId(0);
+        }};
+    }
+
+    @Test
+    public void testEqualsForDifferentTypeOfObject() {
+        Object wrongObject = new Object();
+        assertFalse(transaction.equals(wrongObject));
+    }
+
+    @Test
+    public void testEqualsComparesIds() {
+        Transaction newTransaction = new Transaction() {{
+            setTransactionId(0);
+        }};
+        assertTrue(transaction.equals(newTransaction));
+    }
+
+    @Test
+    public void testEqualsForDifferentIds() {
+        Transaction newTransaction = new Transaction() {{
+            setTransactionId(6);
+        }};
+        assertFalse(transaction.equals(newTransaction));
+    }
+
 }
