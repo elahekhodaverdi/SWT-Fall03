5ab9737f6e2f7322dea608d7cf81929a7dd89cc7
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-11-14 11:59:22 +0330

    improve transaction test

--- Fesadyab/src/test/java/domain/TransactionTest.java
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -1,10 +1,10 @@
 package domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TransactionTest {
     private Transaction transaction;
@@ -17,13 +17,21 @@ public class TransactionTest {
     }
 
     @Test
+    @DisplayName("Test equals with null object")
+    public void testEqualsWithNull() {
+        assertFalse(transaction.equals(null));
+    }
+
+    @Test
+    @DisplayName("Test equals with different type of object")
     public void testEqualsForDifferentTypeOfObject() {
         Object wrongObject = new Object();
         assertFalse(transaction.equals(wrongObject));
     }
 
     @Test
-    public void testEqualsComparesIds() {
+    @DisplayName("Test equals returns true for same IDs, regardless of other fields")
+    public void testEqualsOnlyComparesIds() {
         Transaction newTransaction = new Transaction() {{
             setTransactionId(0);
         }};
@@ -31,11 +39,25 @@ public class TransactionTest {
     }
 
     @Test
+    @DisplayName("Test equals returns false for different IDs")
     public void testEqualsForDifferentIds() {
         Transaction newTransaction = new Transaction() {{
             setTransactionId(6);
         }};
         assertFalse(transaction.equals(newTransaction));
+    }
+
+    @Test
+    @DisplayName("Test getters and setters")
+    public void testGettersAndSetter() {
+        transaction.setTransactionId(1);
+        transaction.setAccountId(2);
+        transaction.setAmount(3);
+        transaction.setDebit(false);
+        assertEquals(1, transaction.getTransactionId());
+        assertEquals(2, transaction.getAccountId());
+        assertEquals(3, transaction.getAmount());
+        assertEquals(false, transaction.isDebit());
     }
 
 }
