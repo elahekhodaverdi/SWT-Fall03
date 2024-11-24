1ebf47a4e7dfe4581910d5285a6af342f6a00c18
MahdiNoori2003 <hitking2007@gmail.com>
2024-11-24 17:34:59 +0330

    add new tests for TableController and fix a bug

--- Mizdooni/src/test/java/mizdooni/controllers/TableControllerTest.java
+++ Mizdooni/src/test/java/mizdooni/controllers/TableControllerTest.java
@@ -1,22 +1,21 @@
 package mizdooni.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import mizdooni.exceptions.*;
 import mizdooni.model.*;
-import mizdooni.response.Response;
 import mizdooni.service.RestaurantService;
 import mizdooni.service.TableService;
 import mizdooni.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalTime;
 import java.util.HashMap;
@@ -24,6 +23,8 @@ import java.util.List;
 import java.util.Map;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,17 +36,14 @@ public class TableControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private UserService userService;
 
-    @Mock
+    @MockBean
     private RestaurantService restaurantService;
 
-    @Mock
+    @MockBean
     private TableService tableService;
-
-    @InjectMocks
-    private TableController tableController;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -62,79 +60,106 @@ public class TableControllerTest {
 
     @Test
     void testGetTablesWithSuccess() throws Exception {
-        when(tableService.getTables(restaurant.getId())).thenReturn(List.of());
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/tables/{restaurantId}", restaurant.getId()))
+        when(tableService.getTables(restaurant.getId())).thenReturn(List.of(table));
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+        mockMvc.perform(get("/tables/{restaurantId}", restaurant.getId()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("tables listed"))
                 .andExpect(jsonPath("$.data").isArray())
                 .andExpect(jsonPath("$.data[0].tableNumber").value(table.getTableNumber()))
                 .andExpect(jsonPath("$.data[0].seatsNumber").value(table.getSeatsNumber()));
     }
-//
-//    @Test
-//    void testGetTablesWhenBadRequest() throws Exception {
-//        when(tableService.getTables(restaurant.getId())).thenThrow(new RuntimeException("test"));
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/tables/{restaurantId}", restaurant.getId()))
-//                .andExpect(status().isBadRequest());
-//        //TODO::have to check error message
-//    }
-//
-//    @Test
-//    void testGetTablesWhenRestaurantNotFound() throws Exception {
-//        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(null);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/tables/{restaurantId}", restaurant.getId()))
-//                .andExpect(status().isBadRequest());
-//        //TODO::have to check error message
-//    }
-//
-//    @Test
-//    void testAddTableWithSuccess() throws Exception {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("seatsNumber", "4");
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/tables/{restaurantId}", restaurant.getId())
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(params)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.message").value("table added"));
-//    }
-//
-//    @Test
-//    void testAddTableWhenMissingSeatsNumber() throws Exception {
-//        Map<String, String> params = new HashMap<>();
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/tables/{restaurantId}", restaurant.getId())
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(params)))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.message").value(ControllerUtils.PARAMS_MISSING));
-//    }
-//
-//    @Test
-//    void testAddTable_Fail_InvalidSeatsNumber() throws Exception {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("seatsNumber", "invalid");
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/tables/{restaurantId}", restaurant.getId())
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(params)))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(jsonPath("$.message").value(ControllerUtils.PARAMS_BAD_TYPE));
-//    }
-//
-//    @Test
-//    void testAddTableWhenInternalError() throws Exception {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("seatsNumber", "4");
-//
-////        when(tableService.addTable(restaurant.getId(), 4)).thenThrow(new RuntimeException("Error"));
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/tables/{restaurantId}", restaurant.getId())
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(params)))
-//                .andExpect(status().isBadRequest());
-//    }
+
+    @Test
+    void testGetTablesWhenBadRequest() throws Exception {
+        when(tableService.getTables(restaurant.getId())).thenThrow(new RuntimeException("test"));
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+
+        mockMvc.perform(get("/tables/{restaurantId}", restaurant.getId()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("test"));
+    }
+
+    @Test
+    void testGetTablesWhenRestaurantNotFound() throws Exception {
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(null);
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(null);
+
+
+        mockMvc.perform(get("/tables/{restaurantId}", restaurant.getId()))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("restaurant not found"));
+    }
+
+    @Test
+    void testAddTableWithSuccess() throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("seatsNumber", "4");
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+
+        mockMvc.perform(post("/tables/{restaurantId}", restaurant.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(params)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("table added"));
+    }
+
+    @Test
+    void testAddTableWhenMissingSeatsNumber() throws Exception {
+        Map<String, String> params = new HashMap<>();
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+
+        mockMvc.perform(post("/tables/{restaurantId}", restaurant.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(params)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ControllerUtils.PARAMS_MISSING));
+    }
+
+    @Test
+    void testAddTableWhenInvalidSeatsNumber() throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("seatsNumber", "invalid");
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+
+        mockMvc.perform(post("/tables/{restaurantId}", restaurant.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(params)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ControllerUtils.PARAMS_BAD_TYPE));
+    }
+
+    @Test
+    void testAddTableWhenUserIsNotManager() throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("seatsNumber", "4");
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+
+        doThrow(new UserNotManager())
+                .when(tableService)
+                .addTable(restaurant.getId(), 4);
+
+        mockMvc.perform(post("/tables/{restaurantId}", restaurant.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(params)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("User is not a manager."));
+    }
+
+    @Test
+    void testAddTableWhenUserIsNotRestaurantManager() throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("seatsNumber", "4");
+        when(restaurantService.getRestaurant(Mockito.anyInt())).thenReturn(restaurant);
+
+        doThrow(new InvalidManagerRestaurant())
+                .when(tableService)
+                .addTable(restaurant.getId(), 4);
+
+        mockMvc.perform(post("/tables/{restaurantId}", restaurant.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(params)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("The manager is not valid for this restaurant."));
+    }
 }
